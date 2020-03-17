8c13767eca751e6a352c433b97999ea5cbdd2f0d
Marcos Sandim <sandim.marcos@gmail.com>
2020-03-17 13:15:03 -0300

    updating

--- scripts/open_scratch.sh
+++ scripts/open_scratch.sh
@@ -3,12 +3,3 @@
 scratchesDir="$HOME/Scratchpads"
 
 ls $scratchesDir | rofi -dmenu | xargs -I{} sh -c "date --rfc-3339=seconds | tr -d '\n' >> $scratchesDir/{}; echo -e ': \n========================\n' >> $scratchesDir/{};  alacritty -e nvim '+normal GA' $scratchesDir/{}"
-
-# extensions=$(cat <<- EXTS
-# sql
-# txt
-# md
-# EXTS
-# )
-#
-# echo "$extensions" | rofi -dmenu | xargs ~/.local/bin/scratchpad
